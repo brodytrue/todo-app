c715c9acc70fe6a42195c8f454c3e780cc1370eb
brodytrue <btm00005@mix.wvu.edu>
2023-11-09 18:31:05 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import "bootstrap/dist/css/bootstrap.css";
+import "./App.css";
+import React, { useState, useRef } from "react";
 
 function App() {
+  const [tasks, setTasks] = useState([]);
+  const taskInputRef = useRef();
+
+  const addTask = () => {
+    const taskText = taskInputRef.current.value;
+    if (taskText.trim() !== "") {
+      setTasks([...tasks, taskText]);
+      taskInputRef.current.value = "";
+    }
+  };
+
+  const removeTask = (index) => {
+    const newTasks = [...tasks];
+    newTasks.splice(index, 1);
+    setTasks(newTasks);
+  };
+
+  const btnStyle = {
+    margin: "5px",
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <h1>To-Do List</h1>
+      <div className="input-container">
+        <input
+          type="text"
+          class="form-control form-control-lg"
+          ref={taskInputRef}
+          placeholder="Add a task"
+        />
+        <button
+          class="btn btn-secondary mb-2"
+          onClick={addTask}
+          style={btnStyle}
         >
-          Learn React
-        </a>
-      </header>
+          Add
+        </button>
+      </div>
+      <ul className="task-container">
+        {tasks.map((task, index) => (
+          <li key={index} className="list-item">
+            {task}
+            <button
+              class="btn btn-secondary mb-2"
+              onClick={() => removeTask(index)}
+              style={btnStyle}
+            >
+              Remove
+            </button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
